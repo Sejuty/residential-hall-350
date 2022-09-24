d4450d99c2d3cb08506116fa5b919dbb0efb65e0
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2022-09-24 17:56:33 +0600

    oi branch merge kor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./Home";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Application from "../src/Application"
 
 function App() {
   return (
+    // issue merge kor
+
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="application" element={<Application />} />
       </Routes>
     </BrowserRouter>
