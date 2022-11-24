92e8f1b9a672d26d01247c44c0ba794786700488
IICTSUST03git <navid26@student.sust.edu>
2022-11-24 12:13:35 +0600

    modified approved issues

--- src/pages/approved-issue/ApprovedIssue.js
+++ src/pages/approved-issue/ApprovedIssue.js
@@ -5,95 +5,78 @@ import { Link, useNavigate } from "react-router-dom";
 import { FcPlus} from "react-icons/fc";
 
 const ApprovedIssue = () => {
-    const issues = [
-        {
-          id: 1,
-          roomNo: 101,
-          regNo: "18BCE1234",
-          issueDate: "2021-01-01",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-          status: "pending",
-          action:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        },
-        {
-          id: 2,
-          roomNo: 102,
-          regNo: "18BCE1235",
-          issueDate: "2021-01-02",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-          status: "completed",
-          action:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        },
-        {
-          id: 3,
-          roomNo: 103,
-          regNo: "18BCE1236",
-          issueDate: "2021-01-03",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-          status: "pending",
-          action:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        },
-        {
-          id: 4,
-          roomNo: 104,
-          regNo: "18BCE1237",
-          issueDate: "2021-01-04",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-          status: "pending",
-          action:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        },
-        {
-          id: 5,
-          roomNo: 105,
-          regNo: "18BCE1238",
-          issueDate: "2021-01-05",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-          status: "working",
-          action:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        },
-        {
-          id: 6,
-          roomNo: 106,
-          regNo: "18BCE1239",
-          issueDate: "2021-01-06",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-          status: "completed",
-          action:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        },
-        {
-          id: 7,
-          roomNo: 107,
-          regNo: "18BCE1240",
-          issueDate: "2021-01-07",
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-          status: "pending",
-          action:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
-        },
-      ];
-    
+     const issues = [
+      {
+        id: 1,
+        roomNo: 101,
+        regNo: "2018831026",
+        issueDate: "2021-01-01",
+        description:"Need a filter",
+        status: "pending",
+        action:""
+      },
+      {
+        id: 2,
+        roomNo: 102,
+        regNo: "2018831090",
+        issueDate: "2021-01-02",
+        description:"Broken Table.",
+        status: "completed",
+        action:"Table has been fixed.",
+      },
+      {
+        id: 3,
+        roomNo: 103,
+        regNo: "201662176",
+        issueDate: "2021-01-03",
+        description:"Need a filter",
+        status: "pending",
+        action:" ",
+      },
+      {
+        id: 4,
+        roomNo: 104,
+        regNo: "2018863326",
+        issueDate: "2021-01-04",
+        description:"Bathroom door is broken",
+        status: "pending",
+        action:"",
+      },
+      {
+        id: 5,
+        roomNo: 105,
+        regNo: "2018337045",
+        issueDate: "2021-01-05",
+        description:"Room door broken",
+        status: "working",
+        action:"note has been issued.",
+      },
+      {
+        id: 6,
+        roomNo: 106,
+        regNo: "2018831876",
+        issueDate: "2021-01-06",
+        description:"Baranda bulb broken.",
+        status: "completed",
+        action:"Bulb has been fixed.",
+      },
+      {
+        id: 7,
+        roomNo: 107,
+        regNo: "2019986378",
+        issueDate: "2021-01-07",
+        description:"Door is broken.",
+        status: "pending",
+        action:"notes has been issed",
+      },
+    ];
 
   const DisplayData = issues.map((info) => {
     return (
       <tr>
-        <td>{info.name}</td>
+        <td>{info.roomNo}</td>
         <td>{info.regNo}</td>
-        <td>{info.dept}</td>
-        <td>{info.semester}</td>
-        <td>{info.merit}</td>
+        <td>{info.issueDate}</td>
         <td>
           <Link to="/expense">
            expenses
@@ -115,12 +98,11 @@ const ApprovedIssue = () => {
         <table className={style.fl_table}>
           <thead>
             <tr>
-              <th>Name</th>
+              <th>RoomNo</th>
               <th>RegNo</th>
-              <th>Department</th>
-              <th>Semester</th>
-              <th>Merit</th>
-              <th>Check Payment</th>
+              <th>issueDate</th>
+              <th>Expenses</th>
+              <th>Reciept</th>
             </tr>
           </thead>
           <tbody>{DisplayData}</tbody>
