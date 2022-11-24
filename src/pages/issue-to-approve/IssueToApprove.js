f7e838ed8b62d07785c39b2609d420cafa7fe997
Sejuty <nishat29@student.sust.edu>
2022-11-24 11:44:05 +0600

    issue data

--- src/pages/issue-to-approve/IssueToApprove.js
+++ src/pages/issue-to-approve/IssueToApprove.js
@@ -13,43 +13,43 @@ const IssueToApprove = () => {
       regNo: "18BCE1234",
       issueDate: "2021-01-01",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Need a filter",
       status: "pending",
       action:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        ""
     },
     {
       id: 2,
       roomNo: 102,
-      regNo: "18BCE1235",
+      regNo: "2018831090",
       issueDate: "2021-01-02",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Broken Table.",
       status: "completed",
       action:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Table has been fixed.",
     },
     {
       id: 3,
       roomNo: 103,
-      regNo: "18BCE1236",
+      regNo: "201662176",
       issueDate: "2021-01-03",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Need a filter",
       status: "pending",
       action:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        " ",
     },
     {
       id: 4,
       roomNo: 104,
-      regNo: "18BCE1237",
+      regNo: "2018863326",
       issueDate: "2021-01-04",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Bathroom door is broken",
       status: "pending",
       action:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "",
     },
     {
       id: 5,
@@ -57,32 +57,32 @@ const IssueToApprove = () => {
       regNo: "18BCE1238",
       issueDate: "2021-01-05",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Room door broken",
       status: "working",
       action:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "note has been issued.",
     },
     {
       id: 6,
       roomNo: 106,
-      regNo: "18BCE1239",
+      regNo: "2018831876",
       issueDate: "2021-01-06",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Baranda bulb broken.",
       status: "completed",
       action:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Bulb has been fixed.",
     },
     {
       id: 7,
       roomNo: 107,
-      regNo: "18BCE1240",
+      regNo: "2019986378",
       issueDate: "2021-01-07",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "Door is broken.",
       status: "pending",
       action:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        "notes has been issed",
     },
   ];
   return (
